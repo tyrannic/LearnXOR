32af70518f61486aa0825e28fb9bf4a0dd588757
dingziyi <161250021@smail.nju.edu.cn>
2019-04-23 12:19:14 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a=15,b=2;
+        System.out.println(a^b);
+        System.out.println(13^13);
+        System.out.println(13^0);
+    }
+}
